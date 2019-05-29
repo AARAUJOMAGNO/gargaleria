5f59349d0e733fd5c2c5e5a5dc7e15fb94e3f665
Gabriel Gil <gabrielbarbosagil@gmail.com>
2019-05-29 16:24:32 -0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,10 @@ seller.save
 
 user = User.first
 
-Beverage.create!(user: seller, name: "Cerva do Frei", price: 27.99, stock: 12, genre: "cerveja", category: "Ale", brand: "Freizão Co.", description: "Uma cerveja encorpada, com alto amargor e leve toque apimentado")
+beverage1 = Beverage.new(user: seller, name: "Cerva do Frei", price: 27.99, stock: 12, genre: "cerveja", category: "Ale", brand: "Freizão Co.", description: "Uma cerveja encorpada, com alto amargor e leve toque apimentado")
+beverage1.remote_photo_url = "https://images.unsplash.com/photo-1549231482-5cf39d19fba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80"
+beverage1.save
 
-Beverage.create!(user: other_seller, name: "Cachaça do Araújo", price: 59.90, stock: 5, genre: "cachaça", category: "Ouro", brand: "Araújo Co.", description: "Envelhecida em barril de carvalho francês por 12 anos")
+beverage2 = Beverage.create!(user: other_seller, name: "Cachaça do Araújo", price: 59.90, stock: 5, genre: "cachaça", category: "Ouro", brand: "Araújo Co.", description: "Envelhecida em barril de carvalho francês por 12 anos")
+beverage2.remote_photo_url = "https://www.esquinademinas.com.br/wp-content/uploads/2016/09/Cachaca-Coracao-de-Minas-Suprema-12-anos-750ml.jpg"
+beverage2.save
