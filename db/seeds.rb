83909114b5c399e49269b150f52718a947b58b49
Gabriel Gil <gabrielbarbosagil@gmail.com>
2019-05-29 16:17:34 -0300

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+
+user = User.new(email: "teste@user.com", password: "123123", terms: true)
+user.save
+
+other_seller = User.new(email: "teste2@seller.com", password: "123123", terms: true, seller: true)
+other_seller.save
+
+seller = User.new(email: "teste@seller.com", password: "123123", terms: true, seller: true)
+seller.save
+
 user = User.first
-Beverage.create!(user: user, name: "Cerva do Frei", price: 27.99, stock: 12, genre: "cerveja", category: "ALE", brand: "Freizão Co.", description: "Uma cerveja encorpada, com alto amargor e leve toque apimentado")
 
-Beverage.create!(user: user, name: "Cachaça do Frei", price: 59.90, stock: 5, genre: "cachaça", category: "Ouro", brand: "Freizão Co.", description: "Envelhecida em barril de carvalho francês por 12 anos")
+Beverage.create!(user: seller, name: "Cerva do Frei", price: 27.99, stock: 12, genre: "cerveja", category: "Ale", brand: "Freizão Co.", description: "Uma cerveja encorpada, com alto amargor e leve toque apimentado")
 
+Beverage.create!(user: other_seller, name: "Cachaça do Araújo", price: 59.90, stock: 5, genre: "cachaça", category: "Ouro", brand: "Araújo Co.", description: "Envelhecida em barril de carvalho francês por 12 anos")
