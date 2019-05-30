c55f53e7103c923200200cc5c723e9876e2d5cff
Gabriel Gil <gabrielbarbosagil@gmail.com>
2019-05-30 17:36:24 -0300

    Show beverage categories based on the radio button selected

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,12 @@ Rails.application.routes.draw do
 
   get 'become_seller', to: "users#become_seller", as: 'become_seller'
   patch 'update_user', to: "users#update_user", as: 'update_user'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :dashboard do
     resources :beverages
     resources :orders, only: %i[show destroy]
+    post 'get_categories', to: "beverages#get_categories", as: "get_categories"
     patch 'orders/:id/shipped', to:"orders#ship_order", as: "ship_order"
     patch 'orders/:id/delivered', to:'orders#delivered', as: 'delivered_order'
   end
