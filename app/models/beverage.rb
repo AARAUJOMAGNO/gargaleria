40a10275d3972028387b1b309f35c8dddf85c357
Gabriel Gil <gabrielbarbosagil@gmail.com>
2019-05-31 14:18:47 -0300

    Add numericality validations to beverage model

--- app/models/beverage.rb
+++ app/models/beverage.rb
@@ -3,8 +3,8 @@ class Beverage < ApplicationRecord
   has_many :orders
 
   validates :name, presence: true
-  validates :price, presence: true
-  validates :stock, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :genre, presence: true, inclusion: { in: %w(cerveja cachaça) }
   validates :category, presence: true
   validates :brand, presence: true
